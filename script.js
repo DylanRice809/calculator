561d5296cb5be44cb24c9f9e0c8643bf9178bb0f
Dylan Rice <dylanpatrickrice@gmail.com>
2023-02-25 19:24:07 +0000

    Add code for completing calculations
    It also connects the buttons to the output screen.

--- script.js
+++ script.js
@@ -1,15 +1,87 @@
+let display = [];
+let displayString = "";
+
 function add (a, b) {
-    return a + b;
+    a = parseInt(a);
+    b = parseInt(b);
+    output = a + b;
+    return output.toString();
 }
 
 function subtract (a, b) {
-    return a - b;
+    a = parseInt(a);
+    b = parseInt(b);
+    output = a - b;
+    return output.toString();
 }
 
 function multiply (a, b) {
-    return a * b;
+    a = parseInt(a);
+    b = parseInt(b);
+    output = a * b;
+    return output.toString();
 }
 
 function divide (a, b) {
-    return a / b;
+    a = parseInt(a);
+    b = parseInt(b);
+    output = a / b;
+    return output.toString();
 }
+
+// do not ask me how or why this works, all I know is that it does
+function computeOutput (input) {
+    let num1 = "";
+    let num2 = "";
+    let operator = "";
+    let output = 0;
+    let firstTime = true;
+    let nextPart = false
+    for (let i = 0; i < input.length + 1; i++) {
+        if ((!(input[i] == "+" || input[i] == "-" || input[i] == "/" || input[i] == "x")) && nextPart == false && firstTime == true) {
+            num1 += input[i];
+        } else if ((input[i] == "+" || input[i] == "-" || input[i] == "/" || input[i] == "x") && firstTime == true && nextPart == false) {
+            operator = input[i];
+            nextPart = true;
+        } else if ((input[i] == "+" || input[i] == "-" || input[i] == "/" || input[i] == "x" || typeof input[i] == "undefined") && ((nextPart == true) || (firstTime == false) || (i == input.length))) {
+            switch (true) {
+                case operator == "+":
+                    output = add(num1, num2);
+                    break;
+                case operator == "-":
+                    output = subtract(num1, num2);
+                    break;
+                case operator == "x":
+                    output = multiply(num1, num2);
+                    break;
+                case operator == "/":
+                    output = divide(num1, num2);
+                    break;
+            }
+            operator = input[i];
+            nextPart = false;
+            firstTime = false;
+            num1 = output;
+            num2 = "";
+        } else {
+            num2 += input[i];
+        }
+    }
+    return output;
+}
+
+const outputDisplay = document.querySelector(".display");
+
+const allButtons = document.querySelectorAll("button");
+for (const button of allButtons) {
+    button.addEventListener("click", () => {
+        if (button.classList.contains("compute")) {
+            console.log(computeOutput(display));
+        } else if (button.classList.contains("delete")) {
+            
+        }
+        display.push(button.innerHTML);
+        displayString += button.innerHTML;
+        outputDisplay.innerHTML = displayString;
+    })
+};
